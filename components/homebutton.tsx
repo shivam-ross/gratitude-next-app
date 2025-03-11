90612b143b82052cca1724bcea0f52f4b790cb05
Shivam Ross <144504100+shivam-ross@users.noreply.github.com>
2025-03-11 18:15:47 +0530

    Update homebutton.tsx

--- components/homebutton.tsx
+++ components/homebutton.tsx
@@ -2,10 +2,10 @@
 import { redirect } from "next/navigation"
 
 export function HomeButton() {
-    return<div>
-        <button className="bg-red-500 text-white text-xl font-semibold py-2 px-4 rounded w-full max-w-md m-2"
+    return<div className="flex flex-col items-center">
+        <button className="bg-red-500 text-white text-xl font-semibold py-2 px-4 rounded w-sm max-w-screen m-2"
         onClick={()=>{redirect("/signup")}}>Get Started</button>
-        <button className="bg-slate-400 text-white text-xl font-semibold py-2 px-4 rounded w-full max-w-md m-2"
+        <button className="bg-slate-400 text-white text-xl font-semibold py-2 px-4 rounded w-sm max-w-screen m-2"
         onClick={()=>{redirect("/signin")}}>I am an Existing User</button>
     </div>
-}
+}}
