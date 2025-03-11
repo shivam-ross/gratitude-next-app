4adccb2ed337229348081ebe22403803dd41bce9
Shivam Ross <144504100+shivam-ross@users.noreply.github.com>
2025-03-11 18:17:55 +0530

    Update homebutton.tsx

--- components/homebutton.tsx
+++ components/homebutton.tsx
@@ -8,4 +8,4 @@ export function HomeButton() {
         <button className="bg-slate-400 text-white text-xl font-semibold py-2 px-4 rounded w-sm max-w-screen m-2"
         onClick={()=>{redirect("/signin")}}>I am an Existing User</button>
     </div>
-}}
+}
